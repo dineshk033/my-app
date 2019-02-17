b4061117c772a6943159c7289262daaeaab71728
dineshk033 <dineshk033@gmail.com>
2019-02-17 20:58:35 +0530

    Updates

---
+++ precache-manifest.ba8b1b98e982ba41c0e863201dc76102.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "caff8f4450a1e5b1c09b",
+    "url": "/my-app/static/js/runtime~main.caff8f44.js"
+  },
+  {
+    "revision": "81f92b7839f5dac1f229",
+    "url": "/my-app/static/js/main.81f92b78.chunk.js"
+  },
+  {
+    "revision": "82b91df31e1ab0c01caa",
+    "url": "/my-app/static/js/2.82b91df3.chunk.js"
+  },
+  {
+    "revision": "81f92b7839f5dac1f229",
+    "url": "/my-app/static/css/main.5a7f1768.chunk.css"
+  },
+  {
+    "revision": "4538745b306d7954feb3ae4b6d4a9a4f",
+    "url": "/my-app/index.html"
+  }
+];
